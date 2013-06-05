bb9b57d21d72d4c0bde6fdfd70e95249212be4c3
Vladyslav Usenko <vsu1991@gmail.com>
2013-06-05 19:10:47 +0200

    added script for fusing several bag files into one multirobot bagfile

---
+++ fuse_rosbags.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rosbag
+import rospy
+import sys
+
+start_time = rospy.Time(1)
+real_start_time = 0
+
+robot_counter = 0
+
+with rosbag.Bag('fused.bag', 'w') as outbag:
+
+    for bag_name in sys.argv[1:]:
+        real_start_time = 0
+        robot_counter += 1
+        prefix = "/cloudbot" + str(robot_counter)
+
+        for topic, msg, t in rosbag.Bag(bag_name).read_messages():
+
+            if real_start_time == 0:
+                real_start_time = t
+
+            if topic == "/tf" and msg.transforms:
+                for i in range(len(msg.transforms)):
+                    msg.transforms[0].header.frame_id = prefix + msg.transforms[0].header.frame_id
+                    msg.transforms[0].child_frame_id = prefix + msg.transforms[0].child_frame_id
+                outbag.write(topic, msg,  start_time + (msg.transforms[0].header.stamp - real_start_time))
+            else:
+                msg.header.frame_id = prefix + msg.header.frame_id
+                outbag.write(prefix + topic, msg, start_time + (msg.header.stamp - real_start_time))
