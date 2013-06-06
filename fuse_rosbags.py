bbde98ab4514ba0e6879529af8c39265d69ebfe1
Vladyslav Usenko <vsu1991@gmail.com>
2013-06-06 17:35:27 +0200

    fixed fimestamps

--- fuse_rosbags.py
+++ fuse_rosbags.py
@@ -4,7 +4,7 @@ import rosbag
 import rospy
 import sys
 
-start_time = rospy.Time(1)
+start_time = rospy.Time.from_sec(1370000000)
 real_start_time = 0
 
 robot_counter = 0
@@ -25,7 +25,9 @@ with rosbag.Bag('fused.bag', 'w') as outbag:
                 for i in range(len(msg.transforms)):
                     msg.transforms[0].header.frame_id = prefix + msg.transforms[0].header.frame_id
                     msg.transforms[0].child_frame_id = prefix + msg.transforms[0].child_frame_id
-                outbag.write(topic, msg,  start_time + (msg.transforms[0].header.stamp - real_start_time))
+		    msg.transforms[0].header.stamp = start_time + (msg.transforms[0].header.stamp - real_start_time) 
+                outbag.write(topic, msg, msg.transforms[0].header.stamp)
             else:
                 msg.header.frame_id = prefix + msg.header.frame_id
-                outbag.write(prefix + topic, msg, start_time + (msg.header.stamp - real_start_time))
+		msg.header.stamp = start_time + (msg.header.stamp - real_start_time)
+                outbag.write(prefix + topic, msg, msg.header.stamp)
