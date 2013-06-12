04cdf2529a56d8826b840c497a07ac85166cfe58
Vladyslav Usenko <vsu1991@gmail.com>
2013-06-12 17:46:52 +0200

    implemented LM for BA

--- rm_parallel/bal.py
+++ rm_parallel/bal.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import pyximport; pyximport.install()
-from jacobian import compute_error_and_jacobian
+from jacobian import compute_error_and_jacobian, compute_error
 import numpy as np
 import cv2
 import scipy.sparse as sp
 from scikits.sparse.cholmod import cholesky
 from scipy.linalg import expm
+from mayavi import mlab
 
 cam_param = 9
 point_param = 3
@@ -19,9 +20,53 @@ print 'Number of cameras', num_cameras
 print 'Number of points', num_points
 print 'Number of observations', num_observations
 
-G_0 = np.array([[0, 0, 0], [0, 0, -1], [0, 1, 0]])
-G_1 = np.array([[0, 0, 1], [0, 0, 0], [-1, 0, 0]])
-G_2 = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 0]])
+
+
+def fill_hessian(cameras, points, observations):
+
+	B_arr = np.zeros((num_cameras, cam_param, cam_param), dtype=np.float64)
+	C_arr = np.zeros((num_points, point_param, point_param), dtype=np.float64)
+	E_arr = np.zeros((num_observations, cam_param, point_param), dtype=np.float64)
+	E_dict = []
+
+	v = np.zeros(num_cameras*cam_param, dtype=np.float64).T
+	w = np.zeros(num_points*point_param, dtype=np.float64).T
+	
+	# Compute error, fill B, C, E
+	error_sum = 0
+	for observation_id in range(num_observations):
+		o = observations[observation_id]
+		cam_id = o['cam_id']
+		point_id = o['point_id']
+		cam = cameras[cam_id]
+		p = points[point_id]
+		
+		R, Jrod = cv2.Rodrigues(cam[0:3])
+		e, JR, Jt, Ji, JX = compute_error_and_jacobian(R, cam[3:6], cam[6:9], p, o['coord'])
+		
+		Jc = np.hstack([np.dot(JR, Jrod.T), Jt, Ji])
+		
+
+		JctJc = np.dot(Jc.T, Jc)
+		JXtJX = np.dot(JX.T, JX)
+				
+		B_arr[cam_id] += JctJc + mu*np.diag(JctJc.diagonal())
+		C_arr[point_id] += JXtJX + mu*np.diag(JXtJX.diagonal())
+
+		E_arr[observation_id] = np.dot(Jc.T, JX)
+		E_dict.append((cam_id, point_id, observation_id))
+	
+		v[cam_id*cam_param:(cam_id+1)*cam_param] += np.dot(Jc.T, e)
+		w[point_id*point_param:(point_id+1)*point_param] += np.dot(JX.T, e)
+	
+		error_sum += (e**2).sum()
+
+	E_dict = sorted(E_dict)
+	E_arr[:] = E_arr[[x[2] for x in E_dict]]
+	
+	return B_arr, C_arr, E_arr, E_dict, v, w, error_sum
+
+
 
 observations = []
 cameras = np.empty((num_cameras, cam_param), dtype=np.float64)
@@ -43,56 +88,44 @@ for i in range(num_points):
 	for j in range(point_param):
 		points[i,j] = float(f.readline())
 
-for i in range(10):
 
-	B_arr = np.zeros((num_cameras, cam_param, cam_param), dtype=np.float64)
-	C_arr = np.zeros((num_points, point_param, point_param), dtype=np.float64)
-	E_arr = np.zeros((num_observations, cam_param, point_param), dtype=np.float64)
-	E_dict = []
-
-	v = np.zeros(num_cameras*cam_param, dtype=np.float64)
-	w = np.zeros(num_points*point_param, dtype=np.float64)
-
-	# Compute error, fill B, C, E
-	error_sum = 0
-	for observation_id in range(num_observations):
-		o = observations[observation_id]
-		cam_id = o['cam_id']
-		point_id = o['point_id']
-		cam = cameras[cam_id]
-		p = points[point_id]
-		R = cv2.Rodrigues(cam[0:3])[0]
-		e, J = compute_error_and_jacobian(R, cam[3:6], cam[6:9], p, o['coord'])
-		JtJ = np.dot(J.T,J)
-		B_arr[cam_id] += JtJ[0:cam_param, 0:cam_param]
-		C_arr[point_id] += JtJ[cam_param:cam_param+point_param, cam_param:cam_param+point_param]
-
-		E_arr[observation_id] = JtJ[0:cam_param, cam_param:cam_param+point_param]
-		E_dict.append((cam_id, point_id, observation_id))
-	
-		Jte = np.dot(J.T,e)
-		v[cam_id*cam_param:(cam_id+1)*cam_param] += Jte[0:cam_param]
-		w[point_id*point_param:(point_id+1)*point_param] += Jte[cam_param:cam_param+point_param]
-	
-		error_sum += e.sum()
+mlab.points3d(points[:,0], points[:,1], points[:,2], mode='point', color=(1,1,1))
+mlab.show()
 
 
-	print error_sum
 
-	#invert C
+
+iteration = 0
+max_iterations = 50
+vv = 2
+mu = 0.01
+
+B_arr, C_arr, E_arr, E_dict, v, w, error_sum = fill_hessian(cameras, points, observations)
+print 'Inital error', error_sum, 'Inital mean error', error_sum/(num_observations*2)
+
+
+while iteration < max_iterations:
+	iteration += 1
+
+	Cp_inv_arr = np.empty_like(C_arr)
+	Bp_arr = np.empty_like(B_arr)
+
+	#compute B+mu*I
+	for i in range(num_cameras):
+		Bp_arr[i] = B_arr[i] + mu * np.eye(cam_param)
+
+	#invert C+mu*I
 	for i in range(num_points):
-		C_arr[i] = np.linalg.inv(C_arr[i])
+		Cp_inv_arr[i] = np.linalg.inv(C_arr[i] + mu * np.eye(point_param))
 
 	indptr = np.arange(num_points+1)
 	indices = np.arange(num_points)
-	C_inv = sp.bsr_matrix((C_arr, indices, indptr), blocksize=(point_param, point_param))
+	C_inv = sp.bsr_matrix((Cp_inv_arr, indices, indptr), blocksize=(point_param, point_param))
 
 	indptr = np.arange(num_cameras+1)
 	indices = np.arange(num_cameras)
-	B = sp.bsr_matrix((B_arr, indices, indptr), blocksize=(cam_param, cam_param))
+	B = sp.bsr_matrix((Bp_arr, indices, indptr), blocksize=(cam_param, cam_param))
 
-	E_dict = sorted(E_dict)
-	E_arr[:] = E_arr[[x[2] for x in E_dict]]
 	indices = [x[1] for x in E_dict]
 	u, indptr = np.unique([x[0] for x in E_dict], return_index=True)
 	indptr = np.hstack([indptr, len(indices)])
@@ -103,23 +136,53 @@ for i in range(10):
 	k = v - EC_inv*w
 
 	factor = cholesky(S)
-	y = factor(k)
-	z = C_inv * (w - (E.T*y).T).T
-	
-	for cam_id in range(num_cameras):
-		cam_update = y[cam_id*cam_param:(cam_id+1)*cam_param]
-		cam = cameras[cam_id]
-		R = cv2.Rodrigues(cam[0:3])[0]
-		dR = expm(G_0 * cam_update[0] + G_1 * cam_update[1] + G_2 * cam_update[2])
-		cam[0:3] = cv2.Rodrigues(np.dot(dR, R))[0].T
-		cam[3:9] = cam[3:9] + cam_update[3:9].T
-		cameras[cam_id] = cam
+	y = factor(k)[:,0]
+	z = C_inv * (w - E.T*y)
 
-	for point_id in range(num_points):
-		point_update = z[point_id*point_param:(point_id+1)*point_param]
-		points[point_id] += point_update[0]
+
+	camera_new = np.empty_like(cameras)
+	points_new = np.empty_like(points)
+	camera_new.flat = cameras.flat - y
+	points_new.flat = points.flat - z
+
+	new_error_sum = 0
+	for observation_id in range(num_observations):
+		o = observations[observation_id]
+		cam_id = o['cam_id']
+		point_id = o['point_id']
+		cam = camera_new[cam_id]
+		p = points_new[point_id]
 		
+		R, Jrod = cv2.Rodrigues(cam[0:3])
+		e = compute_error(R, cam[3:6], cam[6:9], p, o['coord'])
+		
+		new_error_sum += (e**2).sum()
+
+	print 'New error', new_error_sum, 'Mean', new_error_sum/(num_observations*2)
+
+	F_gain = error_sum - new_error_sum
+	L_gain = -(np.dot(y.T,mu*y - v) + np.dot(z.T,mu*z - w))/2
+	sigma = F_gain/L_gain
+	
+	print 'F', F_gain, 'L', L_gain, 'sigma', sigma, 'mu', mu
+
+	if sigma > 0:
+		cameras = camera_new
+		points = points_new
+
+		B_arr, C_arr, E_arr, E_dict, v, w, error_sum = fill_hessian(cameras, points, observations)
+		print 'Iteration', iteration, 'error', error_sum, 'mean error', error_sum/(num_observations*2)
+		
+		mu = mu*max(1.0/3, 1-(2*sigma-1)**3)
+		vv = 2
+	else:
+		mu = mu * vv
+		vv = 2 * vv
 		
 
-	
+
+
+mlab.points3d(points[:,0], points[:,1], points[:,2], mode='point', color=(1,1,1))
+mlab.show()
+
 
--- rm_parallel/symbolic_jacobian.py
+++ rm_parallel/symbolic_jacobian.py
@@ -2,11 +2,6 @@
 from sympy import *
 from sympy.utilities.iterables import flatten
 
-G_0 = Matrix([[0, 0, 0], [0, 0, -1], [0, 1, 0]])
-G_1 = Matrix([[0, 0, 1], [0, 0, 0], [-1, 0, 0]])
-G_2 = Matrix([[0, -1, 0], [1, 0, 0], [0, 0, 0]])
-Jg = Matrix([flatten(G_0), flatten(G_1), flatten(G_2)]).T
-
 R = MatrixSymbol('R', 3,3)
 X = MatrixSymbol('X', 3,1)
 t = MatrixSymbol('t', 3,1)
@@ -21,11 +16,9 @@ p_norm2 = p[0]**2 + p[1]**2
 p_norm4 = p_norm2**2
 p_prime = f * (1 + k1 * p_norm2 + k2 * p_norm4) * p
 
-e = Matrix([(u - p_prime[0])**2, (v -  p_prime[1])**2])
+e = Matrix([u - p_prime[0], v -  p_prime[1]])
 
 JR = e.jacobian(flatten(Matrix(R)))
-Jr = JR*Jg
-
 Jt = e.jacobian(flatten(Matrix(t)))
 JX = e.jacobian(flatten(Matrix(X)))
 Jfk1k2 = e.jacobian([f, k1, k2])
@@ -61,8 +54,17 @@ def compute_error_and_jacobian(R, t, intrinsics, X, coord):
 	cdef double u = coord[0]
 	cdef double v = coord[1]
 
-	J_np = np.empty((2,12), dtype=np.float64)
-	cdef double [:, :] J = J_np
+	JR_np = np.empty((2,9), dtype=np.float64)
+	cdef double [:, :] JR = JR_np
+
+	Jt_np = np.empty((2,3), dtype=np.float64)
+	cdef double [:, :] Jt = Jt_np
+
+	Ji_np = np.empty((2,3), dtype=np.float64)
+	cdef double [:, :] Ji = Ji_np
+
+	JX_np = np.empty((2,3), dtype=np.float64)
+	cdef double [:, :] JX = JX_np
 
 	e_np = np.empty(2, dtype=np.float64)
 	cdef double [:] e = e_np
@@ -72,24 +74,61 @@ print '\te['+str(0)+'] = ' + str(e[0])
 print '\te['+str(1)+'] = ' + str(e[1])
 
 
-for i in range(Jr.shape[0]):
-	for j in range(Jr.shape[1]):
-		print '\tJ['+str(i)+','+str(j)+'] = ' + str(Jr[i,j])
+for i in range(JR.shape[0]):
+	for j in range(JR.shape[1]):
+		print '\tJR['+str(i)+','+str(j)+'] = ' + str(JR[i,j])
 
 for i in range(Jt.shape[0]):
 	for j in range(Jt.shape[1]):
-		print '\tJ['+str(i)+','+str(j+3)+'] = ' + str(Jt[i,j])
+		print '\tJt['+str(i)+','+str(j)+'] = ' + str(Jt[i,j])
 
 for i in range(Jfk1k2.shape[0]):
 	for j in range(Jfk1k2.shape[1]):
-		print '\tJ['+str(i)+','+str(j+6)+'] = ' + str(Jfk1k2[i,j])
+		print '\tJi['+str(i)+','+str(j)+'] = ' + str(Jfk1k2[i,j])
 
 for i in range(JX.shape[0]):
 	for j in range(JX.shape[1]):
-		print '\tJ['+str(i)+','+str(j+9)+'] = ' + str(JX[i,j])
+		print '\tJX['+str(i)+','+str(j)+'] = ' + str(JX[i,j])
 
 
-print "\treturn e_np, J_np"
+print "\treturn e_np, JR_np, Jt_np, Ji_np, JX_np"
 
 
+print """
+
+def compute_error(R, t, intrinsics, X, coord):
+	cdef double R_00 = R[0,0]
+	cdef double R_01 = R[0,1]
+	cdef double R_02 = R[0,2]
+	cdef double R_10 = R[1,0]
+	cdef double R_11 = R[1,1]
+	cdef double R_12 = R[1,2]
+	cdef double R_20 = R[2,0]
+	cdef double R_21 = R[2,1]
+	cdef double R_22 = R[2,2]
+
+	cdef double t_00 = t[0]
+	cdef double t_10 = t[1]
+	cdef double t_20 = t[2]
+
+	cdef double X_00 = X[0]
+	cdef double X_10 = X[1]
+	cdef double X_20 = X[2]
+
+	cdef double f = intrinsics[0]
+	cdef double k1 = intrinsics[1]
+	cdef double k2 = intrinsics[2]
+
+	cdef double u = coord[0]
+	cdef double v = coord[1]
+
+	e_np = np.empty(2, dtype=np.float64)
+	cdef double [:] e = e_np
+"""
+
+print '\te['+str(0)+'] = ' + str(e[0])
+print '\te['+str(1)+'] = ' + str(e[1])
+
+
+print "\treturn e_np"
 
