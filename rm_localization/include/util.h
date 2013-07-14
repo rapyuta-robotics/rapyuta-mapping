19b52c9f19bf93c014c525979a5150fc840f20cb
Vladyslav Usenko <vsu1991@gmail.com>
2013-07-14 17:46:06 +0200

    initial implementaion of localization

---
+++ rm_localization/include/util.h
@@ -0,0 +1,39 @@
+/*
+ * util.h
+ *
+ *  Created on: Jul 14, 2013
+ *      Author: vsu
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <pcl/point_types.h>
+#include <pcl/PolygonMesh.h>
+#include <nav_msgs/OccupancyGrid.h>
+
+#include <octomap/OcTree.h>
+#include <octomap/ColorOcTree.h>
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/publisher.h>
+
+void compute_features(const cv::Mat & rgb, const cv::Mat & depth,
+		const Eigen::Vector4f & intrinsics, cv::Ptr<cv::FeatureDetector> & fd,
+		cv::Ptr<cv::DescriptorExtractor> & de,
+		std::vector<cv::KeyPoint> & filtered_keypoints,
+		pcl::PointCloud<pcl::PointXYZ> & keypoints3d, cv::Mat & descriptors);
+
+bool estimate_transform_ransac(const pcl::PointCloud<pcl::PointXYZ> & src,
+		const pcl::PointCloud<pcl::PointXYZ> & dst,
+		const std::vector<cv::DMatch> matches, int num_iter,
+		float distance2_threshold, int min_num_inliers, Eigen::Affine3f & trans,
+		std::vector<bool> & inliers);
+
+
+#endif /* UTIL_H_ */
